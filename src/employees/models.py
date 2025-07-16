13cd3acbc8eaca873d4d1d8ccf064cdcd469d0be
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-07-16 14:16:05 -0300

    employee model & migration created

--- src/employees/models.py
+++ src/employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    photo = models.ImageField(upload_to="imgs")
+    designation = models.CharField(max_length=128)
+    email_address = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)  # Optional field
+
+    # Creating timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
