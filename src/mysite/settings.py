c97246005bc489b7b29a881f5a711ab00d164eea
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-07-16 14:28:32 -0300

    media upload configurated

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media files configuration (according to the Django documentation)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
