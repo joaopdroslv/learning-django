628cf013b7ca9bdb57f74c5f1a2f6cea12f86d4d
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-07-17 11:24:51 -0300

    template dependency added

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home),
     path("employees/", views.employees),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Default configuration to upload media
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)  # Default configuration to upload media
